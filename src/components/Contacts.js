27788c3c542b7dee011de2880fe3c5633a6a3964
RobertMaina <robertmaina254@gmail.com>
2021-10-25 01:39:31 +0300

    create routes with Link & NavLink

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function Contacts() {
+function Contacts(props) {
+    console.log(props)
     return (
         <div className= "container">
             <h3 className= "center">Contacts</h3>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom'
+
+// replace anchor tags with Link/ NavLink, does the job of preventing default of anchor tags(kind of e preventDefault)
+// NavLink adds class of active. See in inspect browser
 
 const Navbar = () => {
     return (
@@ -6,9 +10,9 @@ const Navbar = () => {
             <div className="container">
                 <a className="brand-logo">Router</a>
                 <ul className= "right">
-                    <li><a href="/home">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contacts">Contacts</a></li>
+                    <li><Link to ="/home">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><NavLink to="/contacts">Contacts</NavLink></li>
                 </ul>
             </div>
         </nav>
